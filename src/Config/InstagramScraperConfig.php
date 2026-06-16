62f19dee89d0bfd906840cbc4899b2f93ad3bad3
Kurusa <kurusa03@gmail.com>
2026-06-16 12:04:51 +0300

    bake static defaults into InstagramScraperConfig

--- src/Config/InstagramScraperConfig.php
+++ src/Config/InstagramScraperConfig.php
@@ -7,16 +7,16 @@ namespace Kurusa\InstagramScraper\Config;
 final readonly class InstagramScraperConfig
 {
     public function __construct(
-        public string $graphqlUrl,
         public string $graphqlCsrfToken,
         public string $graphqlAppId,
         public string $userAgent,
-        public int $graphqlTimeout,
-        public int $requestDelayMicroseconds,
-        public string $profileReelsDocId,
         public int $profileReelsPageSize,
-        public string $profileReelsCursorKey,
         public string $mediaShortcodeDocId,
+        public string $graphqlUrl = 'https://www.instagram.com/graphql/query',
+        public string $profileReelsDocId = '26909206778772295',
+        public string $profileReelsCursorKey = 'max_id',
+        public int $graphqlTimeout = 20,
+        public int $requestDelayMicroseconds = 500000,
     )
     {
     }
